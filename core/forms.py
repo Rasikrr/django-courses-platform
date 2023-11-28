8798ede2c9e99b112f8a7cb942bc41c706c1fadf
Rasikrr <rs.t.95@mail.ru>
2023-11-28 23:29:58 +0600

    profile form page 50%

--- core/forms.py
+++ core/forms.py
@@ -1,19 +1,18 @@
 from django import forms
-from .models import ContactMessage
+from .models import ContactMessage, ProfileModel, CustomUser
 
 
 class ContactForm(forms.ModelForm):
-
     class Meta:
         model = ContactMessage
         fields = ("name", "email", "message", "subject")
         widgets = {
-            "message": forms.Textarea(attrs={"class":"form-control w-100",
-                                          "placeholder": "Enter Message",
-                                          "cols": "30",
-                                          "rows": "9",
-                                          "onfocus": "this.placeholder = ''",
-                                          "onblur": "this.placeholder = 'Enter Message'"
+            "message": forms.Textarea(attrs={"class": "form-control w-100",
+                                             "placeholder": "Enter Message",
+                                             "cols": "30",
+                                             "rows": "9",
+                                             "onfocus": "this.placeholder = ''",
+                                             "onblur": "this.placeholder = 'Enter Message'"
                                              }),
 
             "name": forms.TextInput(attrs={"class": "form-control valid",
@@ -41,3 +40,66 @@ class ContactForm(forms.ModelForm):
     def save(self, commit=True):
         contact_message = super().save(commit=False)
         return contact_message
+
+
+class ProfileForm(forms.ModelForm):
+    first_name = forms.CharField(label="Firstname", widget=forms.TextInput(attrs={"class": "col-sm-9 text-secondary inputs-2",
+                                                                                  "name": "first_name",
+                                                                                  "type": "text",
+                                                                                  "placeholder": "Your name"
+                                                                                  }))
+
+    last_name = forms.CharField(label="Lastname", widget=forms.TextInput(attrs={"class": "col-sm-9 text-secondary inputs-2",
+                                                                                "name": "last_name",
+                                                                                "type": "text",
+                                                                                "placeholder": "Your lastname"
+                                                                                }))
+
+    username = forms.CharField(label="Username", widget=forms.TextInput(attrs={"class": "col-sm-9 text-secondary inputs-2",
+                                                                               "name": "fullname",
+                                                                               "type": "text",
+                                                                               "placeholder": "Your username"
+                                                                               }))
+
+    class Meta:
+        model = ProfileModel
+        fields = ("website", "github", "twitter", "instagram", "facebook", "phone", "address")
+        widgets = {
+            "website": forms.URLInput(attrs={"class": "text-secondary inputs",
+                                             "placeholder": "Your Website",
+                                             "name": "email",
+                                             "type": "url"}),
+
+            "github": forms.URLInput(attrs={"class": "text-secondary inputs",
+                                            "placeholder": "Your Github",
+                                            "name": "github",
+                                            "type": "url"}),
+
+            "twitter": forms.URLInput(attrs={"class": "text-secondary inputs",
+                                             "placeholder": "Your Twitter",
+                                             "name": "twitter",
+                                             "type": "url"}),
+
+            "instagram": forms.URLInput(attrs={"class": "text-secondary inputs",
+                                               "placeholder": "Your Instagram",
+                                               "name": "instagram",
+                                               "type": "url"}),
+
+            "facebook": forms.URLInput(attrs={"class": "text-secondary inputs",
+                                              "placeholder": "Your Facebook",
+                                              "name": "facebook",
+                                              "type": "url"}),
+
+            "phone": forms.NumberInput(attrs={"class": "col-sm-9 text-secondary inputs-2",
+                                              "placeholder": "Your phone",
+                                              "name": "address",
+                                              "type": "number"}),
+
+            "address": forms.TextInput(attrs={"class": "col-sm-9 text-secondary inputs-2",
+                                              "placeholder": "Your address",
+                                              "name": "address",
+                                              "type": "text"
+                                              })
+        }
+
+
--- core/views.py
+++ core/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import redirect, render
 from django.urls import reverse_lazy, reverse
 from django.contrib.auth import login, logout
 from django.contrib.auth.views import LoginView
-from django.views.generic import CreateView, TemplateView, UpdateView
+from django.views.generic import CreateView, TemplateView, UpdateView, FormView
 from django.views import View
 from django.http import JsonResponse, HttpRequest
-from .forms import ContactForm
+from .forms import ContactForm, ProfileForm
 from .models import ContactMessage, CustomUser, ProfileModel
 from mixins import TitleMixin
 
@@ -43,8 +43,9 @@ class Contact(TitleMixin, CreateView):
         return JsonResponse({'message': 'Something went wrong, please try later'})
 
 
-class Profile(TitleMixin, TemplateView):
+class Profile(TitleMixin, FormView):
     title = "Profile | Courses"
+    form_class = ProfileForm
     template_name = "profile.html"
 
     def get_context_data(self, **kwargs):
@@ -55,3 +56,5 @@ class Profile(TitleMixin, TemplateView):
         profile = ProfileModel.objects.get(user=user)
         context["profile"] = profile
         return context
+
+
